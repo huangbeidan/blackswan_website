57af3f3cfe1a4c9f19fc0ab38409c71ce875a142
huangbeidan <huangbeidan@gmail.com>
2022-05-31 16:51:46 -0700

    fix brand name

--- include/navbar.php
+++ include/navbar.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href= <?php echo "$index_url" ?> >Expert Code</a>
+      <a class="navbar-brand" href= <?php echo "$index_url" ?> >Blackswan A.I.</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
